56824c3f20d07abcf3d2f410801fc6b3f91da702
Harpreet Singh <hurpreet@icloud.com>
2023-09-25 01:09:53 -0400

    Lab Completed

--- server/app.py
+++ server/app.py
@@ -4,5 +4,27 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return "<h1>Python Operations with Flask Routing and Views</h1>"
+
+@app.route('/print/<string>')
+def print_string(string):
+    print(string)
+    return string
+
+@app.route('/count/<int:integer>')
+def count(integer):
+    counter = ""
+    for i in range(0, integer):
+        counter += f"{i}\n"
+    return counter
+
+@app.route('/math/<int:num1>/<operation>/<num2>')
+def math(num1, operation, num2):
+    if operation == "div":
+        operation = "/"
+    return str(eval("{}{}{}".format(num1, operation, num2)))
+
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
